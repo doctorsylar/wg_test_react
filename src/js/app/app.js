b3e2097a5311fd58a1fb4df960460b254b731f02
doctorsylar <soul.evans.15@gmail.com>
2021-09-10 22:02:48 +0300

    revert test

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -4,7 +4,6 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
             ships : props.ships,
             activeFilters : {
                 nation : '',
